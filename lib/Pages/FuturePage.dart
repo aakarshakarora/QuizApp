d6f5ecf90ca99af2c644bd93b4267f4972a090c5
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2021-02-10 13:14:04 +0530

    Update FuturePage.dart

--- lib/Pages/FuturePage.dart
+++ lib/Pages/FuturePage.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
 
+class FuturePage extends StatefulWidget {
+  @override
+  _FuturePageState createState() => _FuturePageState();
+}
 
-class FuturePage extends StatelessWidget {
+class _FuturePageState extends State<FuturePage> with WidgetsBindingObserver {
+
+//To Implement Tab Switch Check add this line  " with WidgetsBindingObserver"
+
+//Then add code form line 13 to 51
+   static int pause=0,resume=0,inactive=0,dead=0;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    setState(() {
+      switch(state){
+        case AppLifecycleState.paused:
+          print('paused state');
+          pause++;
+          print(pause);
+          break;
+        case AppLifecycleState.resumed:
+          print('resumed state');
+          resume++;
+          print(resume);
+          break;
+        case AppLifecycleState.inactive:
+          print('inactive state');
+          inactive++;
+          print(inactive);
+          break;
+        case AppLifecycleState.detached:
+          print('suspending state');
+          dead++;
+          print(dead);
+          break;
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,15 +62,49 @@ class FuturePage extends StatelessWidget {
               fontWeight: FontWeight.bold, fontSize: 20),
         ),
       ),
-      body: Center(
-          child: Text(
-            "This Page Will Come in Next Update",
-            style: TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 17,
-                fontWeight: FontWeight.w500,
-                color: Colors.black),
-          )),
+      body: Column(
+        children: [
+          Center(
+              child: Text(
+                "This Page Will Come in Next Update",
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              )),
+
+          Center(
+              child: Text(
+                "Pause State: "+pause.toString(),
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              )),
+
+          Center(
+              child: Text(
+                "Inactive State: "+inactive.toString(),
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              )),
+
+          Center(
+              child: Text(
+                "Resume State: "+resume.toString(),
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black),
+              )),
+        ],
+      ),
     );
   }
 }
