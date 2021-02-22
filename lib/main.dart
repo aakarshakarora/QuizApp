cc0e78197425a0b558018e12fce6678723657d5e
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2021-02-22 23:05:19 +0530

    icon added and  debug banner false

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      debugShowCheckedModeBanner: false,
       home: StartPage(),
     );
   }
