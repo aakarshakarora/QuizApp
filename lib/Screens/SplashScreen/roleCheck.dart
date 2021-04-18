7ee1bf1e691cb0952a079f532d43da3f69db2e31
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2021-04-18 15:34:36 +0530

    persistent login solved

---
+++ lib/Screens/SplashScreen/roleCheck.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:quiz_app/Dashboard/F_Dashboard/dashboardFaculty.dart';
+import 'package:quiz_app/Dashboard/S_Dashboard/dashboardStudent.dart';
+import 'package:quiz_app/Screens/SplashScreen/authHelper.dart';
+import 'package:quiz_app/Screens/SplashScreen/splash.dart';
+import 'package:quiz_app/Screens/Welcome/welcomeScreen.dart';
+
+class RoleCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            UserHelper.saveUser(snapshot.data);
+            return StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("User")
+                  .doc(snapshot.data.uid)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (snapshot.hasData && snapshot.data != null) {
+                  final userDoc = snapshot.data;
+                  final user = userDoc.data();
+                  if (user['Role'] == 'Student') {
+                    return StudentDashboard();
+                  } else if (user['Role'] == 'Faculty') {
+                    return FacultyDashboard();
+                  } else {
+                    return WelcomeScreen();
+                  }
+                } else {
+                  return Material(
+                    child: SplashScreen(),
+                  );
+                }
+              },
+            );
+          }
+          return WelcomeScreen();
+        });
+  }
+}
