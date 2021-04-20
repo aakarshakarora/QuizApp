71b337e1ef6c1ce018f1feee87309fadf9b9da66
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2021-04-20 13:34:14 +0530

    authHelper and Splash Screen added

--- lib/Screens/SplashScreen/splash.dart
+++ lib/Screens/SplashScreen/splash.dart
@@ -3,134 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'file:///D:/Minor%20Project/quiz_app/lib/Screens/Dashboard/F_Dashboard/dashboardFaculty.dart';
-import 'file:///D:/Minor%20Project/quiz_app/lib/Screens/Dashboard/S_Dashboard/dashboardStudent.dart';
-import 'package:quiz_app/Screens/Welcome/welcomeScreen.dart';
 import 'package:quiz_app/Theme/components/background.dart';
 import 'package:quiz_app/Theme/components/fadeAnimation.dart';
 
-//Status: Minor Issues are there
-
-/*
-Persistent Login and Role Check
-*/
-
-class SplashScreen extends StatefulWidget {
-  final String text;
-
-  SplashScreen({@required this.text});
-
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  String _userID;
-  String _switchCode;
-
-  Future<void> startTimer() async {
-    var _duration = Duration(seconds: 5);
-    return Timer(_duration, navigationPage);
-  }
-
-  Future<void> navigationPage() async {
-    switch (_switchCode) {
-      case 'not_logged_in':
-        {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => WelcomeScreen()));
-        }
-        break;
-      case 'not_verified':
-        {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => WelcomeScreen()));
-        }
-        break;
-      case 'Student':
-        {
-          print("User is Student");
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => StudentDashboard()),
-          );
-        }
-        break;
-      case 'Faculty':
-        {
-          print("User is Faculty");
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => FacultyDashboard()),
-          );
-        }
-        break;
-    }
-  }
-
-  void _roleCheck(String userID) {
-    print("inside role check");
-    FirebaseFirestore.instance
-        .collection('User')
-        .doc(userID)
-        .get()
-        .then((value) {
-      if (value.exists) {
-        _switchCode = value.data()['Role'];
-        print("$_switchCode");
-      } else {
-        print("ERROR");
-      }
-    });
-  }
-
-  initState() {
-    print("inside init");
-    super.initState();
-    startTimer();
-    var _user = FirebaseAuth.instance.currentUser;
-    if (_user != null) {
-      _userID = _user.uid;
-      if (_user.emailVerified == true) {
-        print("user id : $_userID");
-        _roleCheck(_userID);
-      } else {
-        _switchCode = 'not_verified';
-        _user.sendEmailVerification();
-      }
-    } else {
-      _switchCode = 'not_logged_in';
-    }
-  }
-
-//  void initState() {
-//    print("inside init");
-//    super.initState();
-//
-//    var _user = FirebaseAuth.instance.currentUser;
-//    if (_user != null) {
-//      if (!_user.emailVerified) {
-//        setState(() {
-//          Navigator.push(
-//            context,
-//            MaterialPageRoute(builder: (context) => LoginPage()),
-//          );
-//        });
-//      } else {
-//        setState(() {
-//          _userID = _user.uid;
-//          print("user id : $_userID");
-//          _roleCheck(_userID);
-//        });
-//        //Verified
-//      }
-//    } else {
-//      _userID = _user.uid;
-//      print("user id : $_userID");
-//      _roleCheck(_userID);
-//      _switchCode = 'not_logged_in';
-//    }
-//  }
+class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -150,7 +26,6 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             Text("Please Wait..."),
-            Text(widget.text),
             Container(
               height: 50,
               width: 50,
